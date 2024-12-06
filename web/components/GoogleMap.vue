14083ae69f9df167ef6f3b0564d520dd7c788e94
Richard Cook <rich@richardsmini10.lan>
2024-12-06 00:01:56 +0000

    Google Maps changes

--- web/components/GoogleMap.vue
+++ web/components/GoogleMap.vue
@@ -6,37 +6,30 @@
 		:fullscreenControl="false"
 		:mapTypeControl="false"
 		:clickableIcons="false"
-		api-key="AIzaSyCU8q_focgghGF1tMuJ2nz9HUaPKbGj-oQ"
 		:style="customInlineStyles"
+		api-key="AIzaSyCU8q_focgghGF1tMuJ2nz9HUaPKbGj-oQ"
+		mapId="3b003f46384c7b3b"
 	>
-		<Circle :options="circleOptions" />
+		<AdvancedMarker :options="markerOptions" :pin-options="pinOptions" />
 	</GoogleMap>
 </template>
 
 <script setup>
 
-import { GoogleMap, Circle } from 'vue3-google-map'
+import { GoogleMap, AdvancedMarker } from 'vue3-google-map'
 
 const props = defineProps({
 	marker: Object,
-	circleFillColor: {
-		type: String,
-		default: '#5f9db4',
-	},
-	circleStrokeColor: {
-		type: String,
-		default: '#5f9db4',
-	},
 })
 
-const circleOptions = {
-	center: props.marker,
-    radius: 250,
-    strokeColor: props.circleStrokeColor,
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: props.circleFillColor,
-    fillOpacity: 0.35,
+const markerOptions = {
+	position: props.marker,
+}
+
+const pinOptions = {
+	background: '#5f9db4',
+	borderColor: '#5f9db4',
+	glyphColor: '#5f9db4',
 }
 
 const customInlineStyles = {
@@ -47,90 +40,6 @@ const customInlineStyles = {
 	'height': 'calc(100% - ((var(--padding-base) * 4) + 28px))',
 	'width': 'calc(100% - (var(--padding-base) * 2))',
 } 
-
-const customStyles = [
-    {
-        "featureType": "administrative",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#444444"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "elementType": "all",
-        "stylers": [
-            {
-                "color": "#e8e7df"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "saturation": "-100"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "all",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 45
-            },
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "transit",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "all",
-		"stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 50
-            },
-        ]
-    }
-]
 
 </script>
 
